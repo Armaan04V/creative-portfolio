f4a828348936e18c287e04f9b9222e4b27654ea6
Armaan Verma <armaan04.v@gmail.com>
2025-10-14 15:44:39 +0530

    scm

--- src/components/Socialmedia.tsx
+++ src/components/Socialmedia.tsx
@@ -23,8 +23,8 @@ import m20 from "@/assets/m20.png";
 import m21 from "@/assets/m21.png";
 
 const images = [
-  m1, m2, m3, m4, m5, m6, m7, m8, m9, m10,
-  m11, m12, m13, m14, m15, m16, m17, m18, m19, m20, m21,
+  m1, m2, m5,  m7, m10,
+  m11, m12, m13, m14, m15, m17, m21,
 ];
 
 const SocialGrid: React.FC = () => {
